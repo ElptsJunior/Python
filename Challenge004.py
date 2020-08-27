5152d4064764ca0d7c778b83ba8e6f5ff3422a5d
Elpts junior <elptsjunior@gmail.com>
2020-08-27 18:50:06 -0300

    This script check's if the value inserted belongs a any "is"condiction

---
+++ Challenge004.py
@@ -0,0 +1,11 @@
+print('='*100)
+print('''BUILD AN SCRIPT THAT INDENTIFY PRYMERY TYPE AND RETURN TO USER'''.title())
+print('='*100)
+
+value = input('Please insert any value : \n')
+print('primary type {}'.format(type(value)))
+print('any spaces ?: {}'.format(value.isspace()))
+print('Is numeric ?: {}'.format(value.isnumeric()))
+print(' is alphanumeric {}'.format(value.isalnum()))
+print('is an upper case {}'.format(value.isupper()))
+
