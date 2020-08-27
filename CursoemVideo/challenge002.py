c34ef1debdcc0d25a80c54eea74f7b6fbca9b66d
Elpts junior <elptsjunior@gmail.com>
2020-08-27 13:24:42 -0300

    This script was build to get the date  information from user and return in brasilian format

---
+++ CursoemVideo/challenge002.py
@@ -0,0 +1,9 @@
+print('='*130)
+print(" BUILD AN PYTHON SCRIPT THAT READ'S DAY,MONTH AND YEAR AND RETURN  THE VALUES ".title())
+print('='*130)
+
+year = int(input('Please insert the year - yyyy :'))
+month = int(input('Now insert the month - mm :'))
+day = int(input('insert your day - dd : '))
+
+print(" the date inserted  {} / {} / {}   isn't ? ".format(day, month,year))
