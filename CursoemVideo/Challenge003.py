8d6eafbc3aefda587134aa665bdac6c97b14a3fe
Elpts junior <elptsjunior@gmail.com>
2020-08-27 13:42:19 -0300

    This script was build to get the summation of two values

---
+++ CursoemVideo/Challenge003.py
@@ -0,0 +1,8 @@
+print('='*130)
+print(''' BUILD AN SCRIPT THAT MAKE AN SUMMATION OF TWO NUMBERS '''.title())
+print('='*130)
+
+value1 = int(input('Please insert the first value : \n'.capitalize()))
+value2 = int(input('insert the second value: \n').capitalize())
+
+print('The result of summation was : {}'.format(value1+value2))
