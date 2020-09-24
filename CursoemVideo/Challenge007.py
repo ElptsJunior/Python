da437207297567a7d7c1397a067eb330056e0549
Windows <elptsjunior@gmail.com>
2020-09-24 00:22:25 -0300

    Challenge 007 now, with colors!

--- CursoemVideo/Challenge007.py
+++ CursoemVideo/Challenge007.py
@@ -1,8 +1,7 @@
-"""
-Build an program that read's two grades of an student,
-and calculate and shows that average
-"""
-print('='*200)
+
+print('\033[0;45m Build an program that reads two grades of an student,and calculate and shows that average\033[m')
+
+print('='*90)
 av1 = (float(input('Welcome again, little Locust !,insert your Grade')))
 av2 = (float(input('Please type the second Grade')))
 avg = (av1 + av2)/2
