987e83ef6b45d676e8a61e6e797258562c470c85
Windows <elptsjunior@gmail.com>
2020-09-24 00:42:40 -0300

    Challenge 010 now, with colors!

--- CursoemVideo/Challenge010.py
+++ CursoemVideo/Challenge010.py
@@ -1,7 +1,7 @@
-'''
-Build an program that ask how much real's do you have and convert to US$
-'''
-real = float(input("what's Real's did you have in your wallet ?  :"))
+
+print('\033[7m Build an program that ask how much reals do you have and convert to US$ \033[m \n')
+
+real = float(input("what's Real's did you have in your wallet ?  : \n").title())
 dollar = 5.25
 cambio = real/dollar
-print('Sorry Little Locust did you have only {:2.3f} USR$ American dollar '.format(cambio))
+print('\033[32m Sorry Little Locust did you have only {:2.3f} USR$ American dollar \033[m'.format(cambio))
