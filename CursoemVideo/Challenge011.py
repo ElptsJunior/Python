3690cf7a7ef16b1ff658bc5cf7f95084db6fe78d
Windows <elptsjunior@gmail.com>
2020-09-24 01:15:05 -0300

    Challenge 011 now, with colors!

--- CursoemVideo/Challenge011.py
+++ CursoemVideo/Challenge011.py
@@ -1,7 +1,7 @@
-'''
-Buil an program that calcules the height and width of the wall in meters,calculate its area and the quantitie
-necessary of paint well khowing that one litre of ink colors 2 meters square  ( 2m²)
-'''
+
+print('\033[7m Build an program that calcules the height and width of the wall in meters,calculate its area and the\033[m')
+print('\033[7m quantitie necessary of paint well khowing that one litre of ink colors 2 meters square  ( 2m²)\033[m\n')
+
 height = float(input('Please insert the height  : '))
 print('Ok, Height of {} meters '.format(height))
 width = float(input('Now insert the width , please .. :'))
