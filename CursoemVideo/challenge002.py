0b1086915755586fe6a2c853f61a13054950d53a
Windows <elptsjunior@gmail.com>
2020-09-23 21:48:22 -0300

    now, with colors!

--- CursoemVideo/challenge002.py
+++ CursoemVideo/challenge002.py
@@ -1,9 +1,9 @@
-print('='*130)
+print('\033[32m = \033[m'*27)
 print(" BUILD AN PYTHON SCRIPT THAT READ'S DAY,MONTH AND YEAR AND RETURN  THE VALUES ".title())
-print('='*130)
+print('\033[32m = \033[m'*27)
 
 year = int(input('Please insert the year - yyyy :'))
 month = int(input('Now insert the month - mm :'))
 day = int(input('insert your day - dd : '))
 
-print(" the date inserted  {} / {} / {}   isn't ? ".format(day, month,year))
+print(' the date inserted  \033[7m{} / {} / {}\033[m isnt ? '.format(day, month, year))
