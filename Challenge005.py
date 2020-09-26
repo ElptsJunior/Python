37feacc36d43fbd766461341f2aaa6491bc37681
Windows <elptsjunior@gmail.com>
2020-09-25 21:58:31 -0300

    Challenge 005 now, with colors!

--- Challenge005.py
+++ Challenge005.py
@@ -1,6 +1,6 @@
-print('='*130)
+print('\033[32m = \033[m'* 25)
 print('build an script that read an number an show your previous and next number:'.title())
-print('='*130)
+print('\033[32m = \033[m'*25)
 
 number = int(input('Insert any number: \n'))
-print(' The inserted number was {} and you previus is : {} and the next valuer is :{}'.format(number, number-1, number+1).capitalize())
+print(' The inserted number was {} and you previus is : \033[4;{} and the next valuer is :{}'.format(number, number-1, number+1).capitalize())
