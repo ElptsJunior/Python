38ef8741115ada3be55e0665758b31e3c616b70f
Windows <elptsjunior@gmail.com>
2020-09-29 22:00:32 -0300

    Challenge 009 , updated with colors

--- CursoemVideo/Challenge009.py
+++ CursoemVideo/Challenge009.py
@@ -3,5 +3,6 @@ number = int(input('Little Locust , insert any number to start the multplication
 index = 1
 while index < 11 :
     result = number*index
-    print('{} x {} = {}'.format(number, index, result))
+    print('\033[31m {}\033[m \033[35m x \033[m \033[32m {:2} \033[m = \033[4;34 m {}\033[m'.format(number, index, result))
+
     index = index + 1
