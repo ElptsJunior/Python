f741bd50223b9f1313da0d11021aa8655023d522
Elpts Ernesto Ribeiro Junior <30675944+ElptsJunior@users.noreply.github.com>
2021-04-01 23:51:58 -0300

    Jogo Joken po com o modulo randint importado da classe randon

--- CursoemVideo/mundo02/Challenge045.py
+++ CursoemVideo/mundo02/Challenge045.py
@@ -1,10 +1,31 @@
 #Joquenpo
+print ("""1 Pedra
+2 papel 
+3 tesoura""")
 
 import random
-selected = str(input('pick an stone, paper or scisor'))
-cpu = random.choice("paper, scisor")
+selected = str(input(' escolha seu numero'))
+print('Jo')
+print("ken")
+print("po")
+opcoes = random.randint(1,3)
+if selected == 1 and opcoes == 1:
+    print("empate")
+elif selected == 1 and opcoes == 2:
+        print("Pedra perde pro papel")
+elif selected == 1 and opcoes == 3:
+    print(" pedra ganha da tesoura")
 
-if  selected == "stone" and   cpu == "paper":
+
+
+
+
+
+
+
+
+"""if  selected == "stone" and   cpu == "paper":
   print("you choose {} and cpu choose {}".format(selected, cpu))
 elif selected in ('scisor') and cpu in ('paper'):
   print("you choose {} and cpu choose {}".format(selected, cpu))
+"""
