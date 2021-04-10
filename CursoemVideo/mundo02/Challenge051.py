9b4300739ff09a9325de86a5b826c36bd3e608a0
Elpts Ernesto Ribeiro Junior <30675944+ElptsJunior@users.noreply.github.com>
2021-04-10 01:25:11 -0300

    Programa que le o primeiro termo e a razao de uma progracao aritimetica , mostrando os 10 promeiros termos da sequencia.

---
+++ CursoemVideo/mundo02/Challenge051.py
@@ -0,0 +1,5 @@
+PrimeiroTermo = int(input("Ola, digite o valor do primeiro termo:"))
+Razao = int(input('adora defina a razao desta Progressao aritimetica:'))
+Termo = PrimeiroTermo * 10
+for index in range(PrimeiroTermo,Termo,Razao):
+    print(index)
