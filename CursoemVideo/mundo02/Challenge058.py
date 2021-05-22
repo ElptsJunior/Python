75d5f93f637ddffaa7fc5762166b4ddd12976b67
Elpts Ernesto Ribeiro Junior <30675944+ElptsJunior@users.noreply.github.com>
2021-05-22 19:30:15 -0300

    add   help  with if more or lass

--- CursoemVideo/mundo02/Challenge058.py
+++ CursoemVideo/mundo02/Challenge058.py
@@ -1,10 +1,14 @@
 from random import randint
 from time import sleep
 ntimes = 1
-cpu = randint(0,11)
+cpu = randint(0,10)
 player = int(input('guess what number between zero until 10 was choosen:  '))
 while player != cpu:
     print('Nop!, try again')
+    if player < cpu:
+        print(' tip ! more..')
+    else:
+        print(' tip less..')
     ntimes += 1
     player = int(input('Guess again what number was choosen by me kk'))
 print('Congratulation !!!!')
