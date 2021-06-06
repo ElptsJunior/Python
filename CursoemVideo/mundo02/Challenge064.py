ff2755f763b7e785994a4d738762d8b60fb38965
Elpts Ernesto Ribeiro Junior <30675944+ElptsJunior@users.noreply.github.com>
2021-06-06 00:55:02 -0300

    made in 10 minutes

---
+++ CursoemVideo/mundo02/Challenge064.py
@@ -0,0 +1,9 @@
+cont = 0
+num = 0
+while num != 999:
+    num = int(input("insert any number, please:  to exit of program type 999 "))
+    num = num
+    cont += 1
+print('end')
+print('''
+the total of number inserted was {}'''.format(cont))
