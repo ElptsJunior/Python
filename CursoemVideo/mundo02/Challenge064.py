60dcb942cfcca4d197c179fab1d02469101b8662
Elpts Ernesto Ribeiro Junior <30675944+ElptsJunior@users.noreply.github.com>
2021-06-08 23:52:24 -0300

    aprendendo a nao contabilizar o flag

--- CursoemVideo/mundo02/Challenge064.py
+++ CursoemVideo/mundo02/Challenge064.py
@@ -1,9 +1,15 @@
 cont = 0
 num = 0
+flag = 0
 while num != 999:
     num = int(input("insert any number, please:  to exit of program type 999 "))
-    num = num
-    cont += 1
+    if num == 999:
+        flag = num
+    else:
+        num = num
+        sume = num + num
+        cont += 1
 print('end')
 print('''
 the total of number inserted was {}'''.format(cont))
+print("total of number was {} ".format(sume))
