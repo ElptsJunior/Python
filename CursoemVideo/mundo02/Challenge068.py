2f8ea06a671a534d917a1659bab74a8d21103f1d
Elpts Ernesto Ribeiro Junior <30675944+ElptsJunior@users.noreply.github.com>
2021-07-14 00:03:44 -0300

    while com break

--- CursoemVideo/mundo02/Challenge068.py
+++ CursoemVideo/mundo02/Challenge068.py
@@ -1,16 +1,24 @@
 from random import randint
 wins = gamble = 0
 while True:
-    gamble = str(input('choose Pair or odd P/O')).split()[0].upper()
+
+    gamble = str(input('choose Pair or odd P/O  :')).split()[0].upper()
+    if gamble != 'P' or 'O':
+        while True:
+            print('incorrect value, Please insert only P / O')
+            gamble = str(input('choose Pair or odd P/0 :')).split()[0].upper()
+            if gamble in 'P' or 'O':
+                break
+    choose = int(input(' insert an number'))
     cpu = randint(1, 100)
-    if gamble in "P" and cpu % 2 == 0 :
-        print(f'You Won ! {cpu} is Pair')
+    if gamble in "P" and choose + cpu % 2 == 0 :
+        print(f'You Won !you put{choose} and  cpu put {cpu} {sum(choose,cpu)} is Pair')
         wins += 1
-    elif gamble in "O" and cpu % 2 == 1:
-        print(f'you won {cpu} is Odd')
+    elif gamble in "O" and choose + cpu % 2 == 1:
+        print(f'you won  you put {choose} and cpu put {cpu}{sum(choose, cpu)} is Odd')
         wins += 1
     else:
-        print('You loose')
+        print('You lose')
         break
 
 print(f'''
---
+++ CursoemVideo/mundo02/Challenge069.py
@@ -0,0 +1,19 @@
+gender = yearold = keep = under20 = over18 = male =  0
+while True:
+    yearold = int(input('How old are you ? '))
+    gender = str(input('Male or female ? M / F')).split()[0].upper()
+    keep = str(input(' did you whant to continue ? Y / N')).split()[0].upper()
+    if keep in 'N':
+        break
+    else:
+        if yearold > 18 :
+            over18 += 1
+        elif gender in 'M':
+            male += 1
+
+        elif gender in 'F' and yearold < 20:
+            under20 += 1
+
+print( f'A) Total of people over 18 years old was { over18 }')
+print(f' B) How manny Men was cadastred ? { male }')
+print(f' c) How manny womens under 20 years old ? {under20} ')
